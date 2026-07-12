566c4ccba721cc5858642f6640adcf77c7d61e8a
grzbpc <grz@grzbpc.co.uk>
2026-07-12 16:15:07 +0100

    Refactor .eleventy.js configuration
    Removed unnecessary module.exports and related configuration from .eleventy.js.

--- .eleventy.js
+++ .eleventy.js
@@ -8,23 +8,6 @@ module.exports = function(eleventyConfig) {
   });
 
   return {
-    module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/css");
-
-    eleventyConfig.addCollection("posts", function(collectionApi) {
-      return collectionApi.getFilteredByGlob("src/posts/*.md").sort((a, b) => {
-        return new Date(b.data.date) - new Date(a.data.date);
-      });
-    });
-
-    return {
-      dir: {
-        input: "src",
-        output: "_site",
-        includes: "_includes"
-      }
-    };
-  };
     dir: {
       input: "src",
       output: "_site",

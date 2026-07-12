21fa905397359320fff6498cf13b7c4b582b3dd2
grzbpc <grz@grzbpc.co.uk>
2026-07-12 16:11:39 +0100

    Update Eleventy config for passthrough and posts collection
    Refactor Eleventy configuration to include passthrough copy and custom collection for posts.

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,23 @@ module.exports = function(eleventyConfig) {
   });
 
   return {
-    pathPrefix: "/citizen-knowledge/",
+    module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/css");
+
+    eleventyConfig.addCollection("posts", function(collectionApi) {
+      return collectionApi.getFilteredByGlob("src/posts/*.md").sort((a, b) => {
+        return new Date(b.data.date) - new Date(a.data.date);
+      });
+    });
+
+    return {
+      dir: {
+        input: "src",
+        output: "_site",
+        includes: "_includes"
+      }
+    };
+  };
     dir: {
       input: "src",
       output: "_site",

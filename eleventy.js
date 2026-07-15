2ee110f4a743e4547bdf75f8c2f83d87f2019227
grzbpc <grz@grzbpc.co.uk>
2026-07-16 00:25:46 +0100

    Add files via upload

---
+++ eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/posts/*.md").sort((a, b) => {
+      const dateDiff = new Date(b.data.date) - new Date(a.data.date);
+      if (dateDiff !== 0) return dateDiff;
+
+      const orderA = (a.data.order !== undefined) ? a.data.order : 999;
+      const orderB = (b.data.order !== undefined) ? b.data.order : 999;
+      return orderA - orderB;
+    });
+  });
+
+  return {
+    pathPrefix: "/citizen-knowledge/",
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes"
+    }
+  };
+};

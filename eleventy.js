7cf59d62be792e4603d1d50580111671911789cb
grzbpc <grz@grzbpc.co.uk>
2026-07-16 01:35:46 +0100

    Add files via upload

--- eleventy.js
+++ eleventy.js
@@ -1,6 +1,14 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
 
+  eleventyConfig.addFilter("dateStr", function(date) {
+    const d = new Date(date);
+    const yyyy = d.getFullYear();
+    const mm = String(d.getMonth() + 1).padStart(2, "0");
+    const dd = String(d.getDate()).padStart(2, "0");
+    return yyyy + "-" + mm + "-" + dd;
+  });
+
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/posts/*.md").sort((a, b) => {
       const dateDiff = new Date(b.data.date) - new Date(a.data.date);

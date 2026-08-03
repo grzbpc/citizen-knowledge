06b2dc779fd3b7747100e98219c4d2521559e604
grzbpc <grz@grzbpc.co.uk>
2026-08-03 04:58:52 +0100

    Add js passthrough copy and path prefix

--- eleventy.js
+++ eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/favicon.ico");
-  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("src/js");
 
   eleventyConfig.addFilter("dateStr", function(date) {
     const d = new Date(date);
@@ -23,6 +22,7 @@ module.exports = function(eleventyConfig) {
   });
 
   return {
+    pathPrefix: "/citizen-knowledge/",
     dir: {
       input: "src",
       output: "_site",

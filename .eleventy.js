3db3e30b3ddd04edf0c83f1db62370432eab0148
grzbpc <grz@grzbpc.co.uk>
2026-08-03 05:08:31 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/apple-touch-icon-180.png");
+  eleventyConfig.addPassthroughCopy("src/js");
 
   eleventyConfig.addFilter("dateStr", function(date) {
     const d = new Date(date);

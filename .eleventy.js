fe2f5e2a3f3bf35f7024623f476e057c4f785801
grzbpc <grz@grzbpc.co.uk>
2026-08-03 04:58:30 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/js");
+
   eleventyConfig.addFilter("dateStr", function(date) {
     const d = new Date(date);
     const yyyy = d.getFullYear();
@@ -8,17 +8,19 @@ module.exports = function(eleventyConfig) {
     const dd = String(d.getDate()).padStart(2, "0");
     return yyyy + "-" + mm + "-" + dd;
   });
+
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/posts/*.md").sort((a, b) => {
       const dateDiff = new Date(b.data.date) - new Date(a.data.date);
       if (dateDiff !== 0) return dateDiff;
+
       const orderA = (a.data.order !== undefined) ? a.data.order : 999;
       const orderB = (b.data.order !== undefined) ? b.data.order : 999;
       return orderA - orderB;
     });
   });
+
   return {
-    pathPrefix: "/citizen-knowledge/",
     dir: {
       input: "src",
       output: "_site",
